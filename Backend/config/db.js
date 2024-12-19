4cb583ca7998eaa2a75e8b96f5103761001a01a3
SubhaNITS150 <subhajyoti_ug_23@cse.nits.ac.in>
2024-12-19 22:21:49 +0530

    Added Frontend

--- Backend/config/db.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose"
-
-export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://quickbite:Subha%232910@cluster0.rstvp.mongodb.net/food-del').then(() => {
-        console.log("DB Connected");
-    })
-}
